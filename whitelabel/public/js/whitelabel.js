ec30584007246fe763f37e8e07af548461b62942
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2021-04-21 18:46:13 +0000

    feat: navbar background color manage from whitelabel setting

--- whitelabel/public/js/whitelabel.js
+++ whitelabel/public/js/whitelabel.js
@@ -6,5 +6,6 @@ $(window).on('load', function() {
         if (frappe.boot.app_logo_details.logo_width) {
             $('.app-logo').css('width',frappe.boot.app_logo_details.logo_width+'px');
         }
+        $('.navbar').css('background-color',frappe.boot.app_logo_details.navbar_background_color)
     })
 })
