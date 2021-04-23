e5858c30e60ded613a6d2700352ec38f84566a17
Bhavesh Maheshwari <34086262+bhavesh95863@users.noreply.github.com>
2021-04-23 15:09:36 +0530

    fix: repeated condition for width

--- whitelabel/public/js/whitelabel.js
+++ whitelabel/public/js/whitelabel.js
@@ -3,8 +3,8 @@ $(window).on('load', function() {
         if (frappe.boot.app_logo_details.logo_width) {
             $('.app-logo').css('width',frappe.boot.app_logo_details.logo_width+'px');
         }
-        if (frappe.boot.app_logo_details.logo_width) {
-            $('.app-logo').css('width',frappe.boot.app_logo_details.logo_width+'px');
+        if (frappe.boot.app_logo_details.logo_height) {
+            $('.app-logo').css('height',frappe.boot.app_logo_details.logo_height+'px');
         }
         $('.navbar').css('background-color',frappe.boot.app_logo_details.navbar_background_color)
         $(`<span style=${frappe.boot.app_logo_details.custom_navbar_title_style.replace('\n','')} class="hidden-xs hidden-sm">${frappe.boot.app_logo_details.custom_navbar_title}</span>`).insertAfter("#navbar-breadcrumbs")
