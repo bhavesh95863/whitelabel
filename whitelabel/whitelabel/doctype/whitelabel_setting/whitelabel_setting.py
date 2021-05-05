df8b98d9df5289db64bb743478a27cccf36e2bd7
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2021-05-05 18:21:33 +0000

    feat: change login title from whitelabel setting page

--- whitelabel/whitelabel/doctype/whitelabel_setting/whitelabel_setting.py
+++ whitelabel/whitelabel/doctype/whitelabel_setting/whitelabel_setting.py
@@ -3,8 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint
+from whitelabel.api import get_frappe_version
 
 class WhitelabelSetting(Document):
-	pass
+	def validate(self):
+		if cint(get_frappe_version()) >= 13:
+			if self.whitelabel_app_name:
+				frappe.db.set_value("System Settings","System Settings","app_name",self.whitelabel_app_name)
+			else:
+				if "erpnext" in frappe.get_installed_apps():
+					frappe.db.set_value("System Settings","System Settings","app_name","ERPNext")
+				else:
+					frappe.db.set_value("System Settings","System Settings","app_name","Frappe")
+
