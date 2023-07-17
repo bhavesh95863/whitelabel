d109f930c09f5c0e5e21e6e111da5bf3faaf5292
rohit.kumar-at-942428920331 <59276987+RohitKumarGit@users.noreply.github.com>
2023-07-17 18:03:04 +0530

    dsdf

--- whitelabel/install.py
+++ whitelabel/install.py
@@ -4,7 +4,6 @@ import re
 def after_install():
     ob_steps = frappe.db.get_all("Onboarding Step")
     brand_name = frappe.get_hooks("brand_name")[0]
-    print(brand_name)
     t = 0
     for step in ob_steps:
         doc = frappe.get_doc("Onboarding Step", step.name, ["title", "description"])
@@ -13,19 +12,16 @@ def after_install():
         if doc.description:
             doc.description = re.sub("ERPNext", brand_name, doc.description)
         doc.save(ignore_permissions=True)
-        if t < 4:
-            print(doc.title, doc.description)
-        t += 1
-    frappe.db.sql(
-        """Update `tabDocField` set label='OneHash' where fieldname='erpnext_user' and parent='Employee'"""
-    )
+        
+    # frappe.db.sql(
+    #     """Update `tabDocField` set label='OneHash' where fieldname='erpnext_user' and parent='Employee'"""
+    # )
     onboard_module_details = frappe.get_all(
         "Module Onboarding", filters={}, fields=["name"]
     )
     for row in onboard_module_details:
         doc = frappe.get_doc("Module Onboarding", row.name)
         doc.title = re.sub("ERPNext", brand_name, doc.title)
-        print(doc.as_dict())
         doc.success_message = re.sub("ERPNext", brand_name, doc.success_message)
         doc.documentation_url = ""
         doc.flags.ignore_mandatory = True
@@ -39,14 +35,16 @@ def after_install():
 
 def whitelabel():
     # set deault otp issuer name
-    sys_setting = frappe.get_single("System Settings")
-    sys_setting.otp_issuer_name = "OneHash"
-    sys_setting.save(ignore_permissions=True)
+    # sys_setting = frappe.get_single("System Settings")
+    # sys_setting.otp_issuer_name = "OneHash"
+    # sys_setting.save(ignore_permissions=True)
+    # set otp_issuer_name in system settings to OneHash
+    frappe.db.set_value("System Settings","System Settings","otp_issuer_name","OneHash" )
     frappe.db.set_value("Website Settings","Website Settings","app_name","OneHash" )
     brand_name = frappe.get_hooks("brand_name")[0]
     frappe.db.set_value("Website Settings","Website Settings","app_name",brand_name )
     frappe.db.set_value("System Settings","System Settings","app_name",brand_name )
-    frappe.db.set_value("Module Def","ERPNext Integrations","module_name","OneHash Integrations" ,update_modified=False)
-    frappe.db.set_value("Module Def","ERPNext Integrations","name","OneHash Integrations" ,update_modified=False)
-
+   # frappe.db.set_value("Module Def","ERPNext Integrations","module_name","OneHash Integrations" ,update_modified=False)
+   # frappe.db.set_value("Module Def","ERPNext Integrations","name","OneHash Integrations" ,update_modified=False)
+    frappe.db.commit()
     
